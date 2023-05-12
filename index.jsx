3537b733406e1075c3c7f6294095744c7e763023
joshua95-web <joshcscurfield@gmail.com>
2023-05-12 12:39:21 +0100

    added the code for homepage function in index.jsx

--- index.jsx
+++ index.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
 
+function Header({ title }) {
+    return <h1>{title ? title : 'Default title'}</h1>;
+}
+
 function HomePage() {
     
 }
 
 export default function HomePage() {
-    
+
 }
