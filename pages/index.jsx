e8f92a4998b668deffbe1eb2f49ba586f0114782
PoshuaUbuntu <joshcscurfield@gmail.com>
2023-05-20 12:13:27 +0100

    updated index.jsx Home function with site title and self description

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,16 @@
 import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
 
-<Head>
-    <title>Joshua Scurfield</title>
-</Head>
-
-import Link from 'next/link';
-
-import { useState } from 'react';
-
-function Header({ title }) {
-    return <h1>{title ? title : 'Default title'}</h1>;
-}
-
-export default function HomePage() {
-
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello, I'm Joshua Scurfield. I'm a guitarist, producer and songwriter. You can stay up to date with my flagship project Shockpowder at shockpowder.com, or stick around here and check out what services I offer as a guitarist and producer.</p>
+      </section>
+    </Layout>
+  );
 }
