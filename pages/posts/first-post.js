eec493574bb456d2539da5ee48655c17833326d4
PoshuaUbuntu <joshcscurfield@gmail.com>
2023-05-20 11:47:19 +0100

    dev console reported a syntax error, recommended changes were applied which caused further errors, reverted to previous syntax and no errors found. Error in question: /> on line 19 of first-post.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -16,7 +16,7 @@ export default function FirstPost() {
         onLoad={() =>
             console.log(`script loaded correctly, window.FB has been populated`)
         }
-        </>
+        />
         <h1>First Post</h1>
         <h2>
             <Link href="/">Back to Home</Link>
